14396f970eb96c123068c2f6f433684acc377ef8
jaroon09et <68173312+jaroon09et@users.noreply.github.com>
2020-08-04 20:23:21 +0700

    talk.py

---
+++ akad/talk.py
@@ -0,0 +1,200 @@
+# -*- coding: utf-8 -*-
+from akad.ttypes import Message
+from random import randint
+
+import json, ntpath
+
+def loggedIn(func):
+    def checkLogin(*args, **kwargs):
+        if args[0].isLogin:
+            return func(*args, **kwargs)
+        else:
+            args[0].callback.other('You want to call the function, you must login to LINE')
+    return checkLogin
+
+class Talk(object):
+    isLogin = False
+    _messageReq = {}
+    _unsendMessageReq = 0
+
+    def __init__(self):
+        self.isLogin = True
+
+    """User"""
+
+    @loggedIn
+    def acquireEncryptedAccessToken(self, featureType=2):
+        return self.talk.acquireEncryptedAccessToken(featureType)
+
+    @loggedIn
+    def getProfile(self):
+        return self.talk.getProfile()
+
+    @loggedIn
+    def getSettings(self):
+        return self.talk.getSettings()
+
+    @loggedIn
+    def getUserTicket(self):
+        return self.talk.getUserTicket()
+
+    @loggedIn
+    def updateProfile(self, profileObject):
+        return self.talk.updateProfile(0, profileObject)
+
+    @loggedIn
+    def updateSettings(self, settingObject):
+        return self.talk.updateSettings(0, settingObject)
+
+    @loggedIn
+    def updateProfileAttribute(self, attrId, value):
+        return self.talk.updateProfileAttribute(0, attrId, value)
+
+    """Operation"""
+
+    @loggedIn
+    def fetchOperation(self, revision, count):
+        return self.talk.fetchOperations(revision, count)
+
+    @loggedIn
+    def getLastOpRevision(self):
+        return self.talk.getLastOpRevision()
+
+    """Message"""
+
+    @loggedIn
+    def sendMessage(self, to, text, contentMetadata={}, contentType=0):
+        msg = Message()
+        msg.to, msg._from = to, self.profile.mid
+        msg.text = text
+        msg.contentType, msg.contentMetadata = contentType, contentMetadata
+        if to not in self._messageReq:
+            self._messageReq[to] = -1
+        self._messageReq[to] += 1
+        return self.talk.sendMessage(self._messageReq[to], msg)
+    
+    """ Usage:
+        @to Integer
+        @text String
+        @dataMid List of user Mid
+    """
+    
+    @loggedIn
+    def sendText(self, Tomid, text):
+        msg = Message()
+        msg.to = Tomid
+        msg.text = text
+
+        return self.talk.sendMessage(0, msg)
+        
+    @loggedIn
+    def sendMessageWithMention(self, to, mid, firstmessage, lastmessage):
+        try:
+            arrData = ""
+            text = "%s " %(str(firstmessage))
+            arr = []
+            mention = "@x "
+            slen = str(len(text))
+            elen = str(len(text) + len(mention) - 1)
+            arrData = {'S':slen, 'E':elen, 'M':mid}
+            arr.append(arrData)
+            text += mention + str(lastmessage)
+            self.sendMessage(to,text, {'MENTION': str('{"MENTIONEES":' + json.dumps(arr) + '}')}, 0)
+        except Exception as error:
+            print(error)
+
+    @loggedIn
+    def sendSticker(self, to, packageId, stickerId):
+        contentMetadata = {
+            'STKVER': '100',
+            'STKPKGID': packageId,
+            'STKID': stickerId
+        }
+        return self.sendMessage(to, '', contentMetadata, 7)
+        
+    @loggedIn
+    def sendContact(self, to, mid):
+        contentMetadata = {'mid': mid}
+        return self.sendMessage(to, '', contentMetadata, 13)
+
+    @loggedIn
+    def sendGift(self, to, productId, productType):
+        if productType not in ['theme','sticker']:
+            raise Exception('Invalid productType value')
+        contentMetadata = {
+            'MSGTPL': str(randint(0, 12)),
+            'PRDTYPE': productType.upper(),
+            'STKPKGID' if productType == 'sticker' else 'PRDID': productId
+        }
+        return self.sendMessage(to, '', contentMetadata, 9)
+
+    @loggedIn
+    def sendMessageAwaitCommit(self, to, text, contentMetadata={}, contentType=0):
+        msg = Message()
+        msg.to, msg._from = to, self.profile.mid
+        msg.text = text
+        msg.contentType, msg.contentMetadata = contentType, contentMetadata
+        if to not in self._messageReq:
+            self._messageReq[to] = -1
+        self._messageReq[to] += 1
+        return self.talk.sendMessageAwaitCommit(self._messageReq[to], msg)
+
+    @loggedIn
+    def unsendMessage(self, messageId):
+        self._unsendMessageReq += 1
+        return self.talk.unsendMessage(self._unsendMessageReq, messageId)
+
+    @loggedIn
+    def requestResendMessage(self, senderMid, messageId):
+        return self.talk.requestResendMessage(0, senderMid, messageId)
+
+    @loggedIn
+    def respondResendMessage(self, receiverMid, originalMessageId, resendMessage, errorCode):
+        return self.talk.respondResendMessage(0, receiverMid, originalMessageId, resendMessage, errorCode)
+
+    @loggedIn
+    def removeMessage(self, messageId):
+        return self.talk.removeMessage(messageId)
+    
+    @loggedIn
+    def removeAllMessages(self, lastMessageId):
+        return self.talk.removeAllMessages(0, lastMessageId)
+
+    @loggedIn
+    def removeMessageFromMyHome(self, messageId):
+        return self.talk.removeMessageFromMyHome(messageId)
+
+    @loggedIn
+    def destroyMessage(self, chatId, messageId):
+        return self.talk.destroyMessage(0, chatId, messageId, sessionId)
+    
+    @loggedIn
+    def sendChatChecked(self, consumer, messageId):
+        return self.talk.sendChatChecked(0, consumer, messageId)
+
+    @loggedIn
+    def sendEvent(self, messageObject):
+        return self.talk.sendEvent(0, messageObject)
+
+    @loggedIn
+    def getLastReadMessageIds(self, chatId):
+        return self.talk.getLastReadMessageIds(0, chatId)
+
+    @loggedIn
+    def getPreviousMessagesV2WithReadCount(self, messageBoxId, endMessageId, messagesCount=50):
+        return self.talk.getPreviousMessagesV2WithReadCount(messageBoxId, endMessageId, messagesCount)
+
+    """Object"""
+
+    @loggedIn
+    def sendImage(self, to, path):
+        objectId = self.sendMessage(to=to, text=None, contentType = 1).id
+        return self.uploadObjTalk(path=path, type='image', returnAs='bool', objId=objectId)
+
+    @loggedIn
+    def sendImageWithURL(self, to, url):
+        path = self.downloadFileURL(url, 'path')
+        return self.sendImage(to, path)
+
+    @loggedIn
+    def sendGIF(self, to, path):
